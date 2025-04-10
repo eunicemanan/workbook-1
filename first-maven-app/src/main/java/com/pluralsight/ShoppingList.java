6186edbd6d94dd4f7995bdedd35cc77705856cb1
Eunice_Manan <mananeunice@gmail.com>
2025-04-10 11:19:19 -0400

    completed ShoppingList app

---
+++ first-maven-app/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Hoison Sauce");
+        System.out.println("Fish Sauce");
+        System.out.println("Pork Belly");
+        System.out.println("Chilli Peppers");
+        System.out.println("Garlic");
+        System.out.println("Rice");
+        System.out.println("Bok Choy");
+        System.out.println("Lettuce");
+        System.out.println("Sesame Oil");
+        System.out.println("Cheese");
+
+    }
+}
